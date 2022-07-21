2a3ef1bff6130c7127c266cbb1cb63e555b4dfcd
kkhoppe <109746150+kkhoppe@users.noreply.github.com>
2022-07-21 18:57:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-var http = require('http');
+/*var http = require('http');
 var dt = require('./myfirstmodule');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Hello World!' + dt.myDateTime());
     res.end();
 }).listen(process.env.PORT || 3000);
+*/
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  //Return the url part of the request object:
+  res.write(req.url);
+  res.end();
+}).listen(8080);
