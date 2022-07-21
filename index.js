414e55898ae0909f4f329ac2c458a5241603090a
kkhoppe <109746150+kkhoppe@users.noreply.github.com>
2022-07-21 18:48:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 var dt = require('./myfirstmodule');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Hello World!' + dt.myDateTime());
+    res.write('Hello World!' + dt.myDateTime() + ${req.url});
     res.end();
 }).listen(process.env.PORT || 3000);
